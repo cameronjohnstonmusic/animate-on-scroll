19b462623f4db809e001b4da4fa7a817952e914c
cameronjohnstonmusic <cameronjohnston@icloud.com>
2023-01-23 20:16:37 -0800

    remove init function

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 
-function init() {
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
 
-            console.log(entry)
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            } else {
-                entry.target.classList.remove('show');
-            }
-
-        });
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
 
     });
 
+});
 
-    const hiddenElements = document.querySelectorAll('.hidden');
-    hiddenElements.forEach((el) => observer.observe(el));
 
-}
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
